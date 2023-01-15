1bfd644c6e7e1a9c185af8db7dfa8e3b20ef8358
Samantha Dhaussy <sj.dhaussy@gmail.com>
2023-01-15 17:18:45 +0100

    MISSION 2 : Tâche 1 - Gérer les formations
    Une page doit permettre de lister les formations et, pour chaque formation, afficher un bouton permettant de la supprimer (après confirmation) et un bouton permettant de la modifier.
    
    Si une formation est supprimée, il faut aussi l'enlever de la playlist où elle se trouvait.
    
    Les mêmes tris et filtres présents dans le front office doivent être présents dans le back office.
    
    Un bouton doit permettre d'accéder au formulaire d'ajout d'une formation.
    Les saisies doivent être contrôlées. Seul le champ "description" n'est pas obligatoire ainsi que la sélection de catégories (une formation peut n'avoir aucune catégorie). 
    
    La playlist et la ou les catégories doivent être sélectionnées dans une liste (une seule playlist par formation, plusieurs catégories possibles par formation). La date ne doit pas être saisie mais sélectionnée. Elle ne doit pas être postérieure à la date du jour.
    
    Le clic sur le bouton permettant de modifier une formation doit amener sur le même formulaire, mais cette fois prérempli.
    Task #4 - MISSION 2 : Tâche 1 - Gérer les formations (5h)

---
+++ src/Controller/admin/AdminFormationsController.php
@@ -0,0 +1,159 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller\admin;
+
+use App\Entity\Formation;
+use App\Form\FormationType;
+use App\Repository\CategorieRepository;
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AdminFormationsController
+ *
+ * @author samsam
+ */
+class AdminFormationsController extends AbstractController {
+    
+    /**
+     * 
+     * @var FormationRepository
+     */
+    private $formationRepository;
+    
+    /**
+     * 
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+    
+    /**
+     * Création du constructeur
+     * @param FormationRepository $formationRepository
+     */
+    function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+        $this->formationRepository = $formationRepository;
+        $this->categorieRepository = $categorieRepository;
+    }
+    
+    /**
+     * @Route("/admin", name="admin.formations")
+     * @return Response
+     */
+    public function index(): Response{
+        $formations = $this->formationRepository->findAllOrderBy('title', 'ASC');
+        $categories = $this->categorieRepository->findAll();
+        return $this->render("admin/admin.formations.html.twig", [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }
+    
+    /**
+     * Suppression d'une formation
+     * @Route("/admin/suppr.formation/{id}", name="admin.suppr.formation")
+     * @param Formation $formations
+     * @return Response
+     */
+    public function suppr(Formation $formations): Response{
+        $this->formationRepository->remove($formations, true);
+        return $this->redirectToRoute('admin.formations');
+    }
+    
+    /**
+     * Edition d'une formation
+     * @Route("/admin/edit/{id}", name="admin.edit.formations")
+     * @param Formation $formations
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Formation $formations, Request $request): Response{
+        $formFormation = $this->createForm(FormationType::class, $formations);
+        
+        $formFormation->handleRequest($request);
+        if($formFormation->isSubmitted() && $formFormation->isValid()){
+            $this->formationRepository->add($formations, true);
+            return $this->redirectToRoute('admin.formations');
+        }
+        
+        return $this->render("admin/admin.edit.formations.html.twig", [
+            'formations' => $formations,
+            'formformation' => $formFormation->createView()
+        ]);
+    }
+    
+    /**
+     * Ajout d'une formation
+     * @Route("/admin/ajout", name="admin.ajout.formations")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response{
+        $formations = new Formation();
+        $formFormation = $this->createForm(FormationType::class, $formations);
+        
+        $formFormation->handleRequest($request);
+        if($formFormation->isSubmitted() && $formFormation->isValid()){
+            $this->formationRepository->add($formations, true);
+            return $this->redirectToRoute('admin.formations');
+        }
+        
+        return $this->render("admin/admin.ajout.formations.html.twig", [
+            'formations' => $formations,
+            'formformation' => $formFormation->createView()                
+        ]);
+    }
+    
+     /**
+     * Retourne toutes les formations triées sur un champ
+     * Et sur un champ si autre table
+     * @Route("/admin/formations/tri/{champ}/{ordre}/{table}", name="admin.formations.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @param type $table
+     * @return Response
+     */
+    public function sort($champ, $ordre, $table=""): Response{
+        if($table != ""){
+            $formations = $this->formationRepository->findAllOrderByTable($champ, $ordre, $table);
+        }else{
+            $formations = $this->formationRepository->findAllOrderBy($champ, $ordre);
+        }
+        $categories = $this->categorieRepository->findAll();
+        return $this->render('admin/admin.formations.html.twig', [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }
+    
+    /**
+     * @Route("/admin/formations/recherche/{champ}/{table}", name="admin.formations.findallcontain")
+     * @param type $champ
+     * @param Request $request
+     * @param type $table
+     * @return Response
+     */
+    public function findAllContain($champ, Request $request, $table=""): Response{
+        $valeur = $request->get("recherche");
+        if($table !=""){
+            $formations = $this->formationRepository->findByContainValueTable($champ, $valeur, $table);
+        }else{
+            $formations = $this->formationRepository->findByContainValue($champ, $valeur);
+        }
+        $categories = $this->categorieRepository->findAll();
+        return $this->render('admin/admin.formations.html.twig', [
+            'formations' => $formations,
+            'categories' => $categories,
+            'valeur' => $valeur,
+            'table' => $table
+        ]);
+    }  
+}
---
+++ src/Form/FormationType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Playlist;
+use DateTime;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Description of FormationType
+ *
+ * @author samsam
+ */
+class FormationType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+                ->add('title', TextType::class, [
+                    'label' => 'Formation',
+                    'required' => true])
+                ->add('description', TextareaType::class, [
+                    'label' => 'Description',
+                    'required' => false
+                ])
+                ->add('playlist', EntityType::class, [
+                    'class' => Playlist::class,
+                    'label' => 'Playlist',
+                    'choice_label' => 'name',
+                    'multiple' => false,
+                    'required' => true
+                ])
+                ->add('categories', EntityType::class, [
+                'class' => Categorie::class,
+                'label' => 'Catégorie',
+                'choice_label' => 'name',
+                'multiple' => true,
+                'required' => false
+                ])
+                ->add('publishedAt', DateType::class, [
+                    'widget' => 'single_text',
+                    'data' => isset($options['data']) && $options['data']->getPublishedAt() != null ? $options['data']->getPublishedAt() : new DateTime('now'),
+                    'label' => 'Date',
+                    'required' => true
+                ])
+                ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer'
+                ]);     
+    }
+}
